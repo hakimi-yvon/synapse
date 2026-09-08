1ae88df5cfd3715a3e6379f14e6db6b2769ca261
hakimi-yvon <yvonndouanla@gmail.com>
2026-09-08 03:14:59 +0100

    Add core models, admin registration, and superuser setup

--- digests/admin.py
+++ digests/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Source, Article, Topic, UserPreference, DeliveryChannel, Digest, DigestDelivery
 
-# Register your models here.
+admin.site.register(Source)
+admin.site.register(Article)
+admin.site.register(Topic)
+admin.site.register(UserPreference)
+admin.site.register(DeliveryChannel)
+admin.site.register(Digest)
+admin.site.register(DigestDelivery)

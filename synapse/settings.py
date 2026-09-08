46dc76786b45a5605de6840288ad737e0b26bfc0
hakimi-yvon <yvonndouanla@gmail.com>
2026-09-08 03:21:44 +0100

    Configure Celery with Redis broker

--- synapse/settings.py
+++ synapse/settings.py
@@ -139,3 +139,9 @@ MAILERS = {
         'BACKEND': 'django.core.mail.backends.console.EmailBackend',
     },
 }
+
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'Africa/Douala'
